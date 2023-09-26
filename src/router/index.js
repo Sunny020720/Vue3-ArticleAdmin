276ca4c7bcd6a56df068805c2271c49f5042dd2d
Sunny020720 <1538052461@qq.com>
2023-09-26 15:13:55 +0800

    准备工作-VueRouter4

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// createRouter 创建路由实例
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // 地址栏不带 #
+  // createHashHistory 带#
   routes: []
 })
-
 export default router
