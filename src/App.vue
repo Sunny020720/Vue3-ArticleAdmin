bb01814edcd2ac10d345df2f8b779fa96dcac9f1
Sunny020720 <1538052461@qq.com>
2023-09-26 15:13:32 +0800

    准备工作-引入elementplus-按需导入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<script setup></script>
-
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+//1.获取路由对象
+const router = useRouter()
+//2.获取路由参数
+const route = useRoute()
+const goList = () => {
+  router.push('/list')
+  console.log(router, route)
+}
+</script>
 <template>
-  <div>我是App</div>
+  <div>
+    我是App
+    <el-button @click="$router.push('home')">跳首页</el-button>
+    <el-button @click="goList">跳列表页</el-button>
+  </div>
 </template>
