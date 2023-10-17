ddc43e5d1a8f685cd7f8f63e57f1243ee108eb6c
Sunny020720 <1538052461@qq.com>
2023-10-18 00:05:26 +0800

    页面-文章分类-表格动态渲染

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -1,5 +1,61 @@
-<script setup></script>
+<script setup>
+import PageContainer from '@/components/PageContainer.vue'
+import { artGetChannelsService } from '@/api/article'
+import { ref } from 'vue'
+import { Delete, Edit } from '@element-plus/icons-vue'
 
-<template>频道管理</template>
+//3.loading效果 见1.
+const loading = ref(false)
 
-<style scoped></style>
+//1.获取频道列表
+const channelList = ref([])
+const getChannelList = async () => {
+  loading.value = true // 3.发请求前开启
+  const res = await artGetChannelsService()
+  channelList.value = res.data.data
+  loading.value = false // 3.请求结束关闭
+}
+getChannelList()
+
+//2.表格操作
+const onEditChannel = (row) => {
+  console.log(row)
+}
+const onDelChannel = (row) => {
+  console.log(row)
+}
+</script>
+
+<template>
+  <page-container title="文章分类">
+    <template #extra>
+      <el-button type="primary"> 添加分类 </el-button>
+    </template>
+    <el-table v-loading="loading" :data="channelList" style="width: 100%">
+      <el-table-column label="序号" type="index" width="100"> </el-table-column>
+      <el-table-column label="分类名称" prop="cate_name"></el-table-column>
+      <el-table-column label="分类别名" prop="cate_alias"></el-table-column>
+      <el-table-column label="操作" width="100">
+        <template #default="{ row }">
+          <el-button
+            :icon="Edit"
+            circle
+            plain
+            type="primary"
+            @click="onEditChannel(row)"
+          ></el-button>
+          <el-button
+            :icon="Delete"
+            circle
+            plain
+            type="danger"
+            @click="onDelChannel(row)"
+          ></el-button>
+        </template>
+      </el-table-column>
+      <template #empty>
+        <el-empty description="没有数据" />
+      </template>
+    </el-table>
+  </page-container>
+</template>
