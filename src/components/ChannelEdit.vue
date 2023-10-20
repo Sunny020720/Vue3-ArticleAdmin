f73d6f704c7ea48181a7f1ca9d5df3e5e356cf1f
Sunny020720 <1538052461@qq.com>
2023-10-20 21:16:28 +0800

    组件-channel-弹窗(编辑和添加)-表单内容

--- src/components/ChannelEdit.vue
+++ src/components/ChannelEdit.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+//1.
 const dialogVisible = ref(false) // 表单是否显示
 // 打开弹窗
 // open({}) => 表单无需渲染,说明是添加
@@ -7,16 +8,64 @@ const dialogVisible = ref(false) // 表单是否显示
 const open = async (row) => {
   dialogVisible.value = true
   console.log(row)
+  formModel.value = { ...row } // 添加 => 重置数据 ; 编辑 => 回显数据
 }
 // 组件对外暴露方法
 defineExpose({
   open
 })
+
+//2.表单内容
+const formModel = ref({
+  cate_name: '',
+  cate_alias: ''
+})
+const rules = {
+  cate_name: [
+    { required: true, message: '请输入分类名称', trigger: 'blur' },
+    {
+      pattern: /^\S{1,10}$/,
+      message: '必须为1-10位非空字符',
+      trigger: 'blur'
+    }
+  ],
+  cate_alias: [
+    { required: true, message: '请输入分类别名', trigger: 'blur' },
+    {
+      pattern: /^[a-zA-Z0-9]{1,15}$/,
+      message: '必须为1-15位字母或数字',
+      trigger: 'blur'
+    }
+  ]
+}
 </script>
 
 <template>
-  <el-dialog v-model="dialogVisible" title="添加弹层" width="30%">
-    <div>我是内容部分</div>
+  <el-dialog
+    v-model="dialogVisible"
+    :title="formModel.id ? '编辑分类' : '添加分类'"
+    width="30%"
+  >
+    <el-form
+      :model="formModel"
+      :rules="rules"
+      label-width="100px"
+      style="padding-right: 30px"
+    >
+      <el-form-item label="分类名称" prop="cate_name">
+        <el-input
+          v-model="formModel.cate_name"
+          placeholder="请输入分类名称"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="分类别名" prop="cate_alias">
+        <el-input
+          v-model="formModel.cate_alias"
+          placeholder="请输入分类别名"
+        ></el-input>
+      </el-form-item>
+    </el-form>
+
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
