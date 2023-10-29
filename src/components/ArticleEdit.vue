ca6d159ef8598146cd43549393c34b07fbee4ba4
Sunny020720 <1538052461@qq.com>
2023-10-29 18:16:08 +0800

    组件-文章管理-抽屉

---
+++ src/components/ArticleEdit.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+const visibleDrawer = ref(false) //控制抽屉显示
+//打开组件
+//open({}) => 添加：表单无需渲染
+//open({ id, ...}) => 编辑：表单需要渲染
+const open = (row) => {
+  visibleDrawer.value = true
+  console.log(row)
+}
+
+defineExpose({
+  open
+})
+</script>
+
+<template>
+  <el-drawer v-model="visibleDrawer" direction="rtl" size="70%" title="大标题">
+    el-drawer
+  </el-drawer>
+</template>
