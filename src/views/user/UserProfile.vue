b6409b983d74ee9cbd0fe1be7bf23a34565bbb09
Sunny020720 <1538052461@qq.com>
2023-11-09 18:19:54 +0800

    页面-用户-基本信息

--- src/views/user/UserProfile.vue
+++ src/views/user/UserProfile.vue
@@ -1,5 +1,69 @@
-<script setup></script>
+<script setup>
+import PageContainer from '@/components/PageContainer.vue'
+import { useUserStore } from '@/stores'
+import { ref } from 'vue'
+import { userUpdateInfoService } from '@/api/user'
 
-<template>个人中心</template>
+//用户信息
+const {
+  user: { username, nickname, email, id },
+  getUser
+} = useUserStore()
+const userInfo = ref({ username, nickname, email, id })
+// 校验规则
+const rules = {
+  nickname: [
+    { required: true, message: '请输入用户昵称', trigger: 'blur' },
+    {
+      pattern: /^\S{2,10}$/,
+      message: '昵称必须是2-10位的非空字符串',
+      trigger: 'blur'
+    }
+  ],
+  email: [
+    { required: true, message: '请输入用户邮箱', trigger: 'blur' },
+    { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
+  ]
+}
+
+// 提交
+const formRef = ref()
+const onSubmit = async () => {
+  await formRef.value.validate()
+  await userUpdateInfoService(userInfo.value)
+  await getUser()
+  ElMessage.success('修改成功')
+}
+</script>
+
+<template>
+  <page-container title="基本资料">
+    <el-row>
+      <el-col :span="10">
+        <!--    表单区域-->
+        <el-form
+          ref="formRef"
+          :model="userInfo"
+          :rules="rules"
+          label-width="80px"
+          size="large"
+        >
+          <el-form-item label="登录名称">
+            <el-input v-model="userInfo.username" disabled></el-input>
+          </el-form-item>
+          <el-form-item label="用户昵称" prop="nickname">
+            <el-input v-model="userInfo.nickname"></el-input>
+          </el-form-item>
+          <el-form-item label="用户邮箱" prop="email">
+            <el-input v-model="userInfo.email"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">提交修改</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </page-container>
+</template>
 
 <style scoped></style>
