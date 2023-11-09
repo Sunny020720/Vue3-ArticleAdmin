f00030263b1e0a3c736dc61e7e1d2b7250265319
Sunny020720 <1538052461@qq.com>
2023-11-09 17:52:56 +0800

    api-用户-更新信息

--- src/api/user.js
+++ src/api/user.js
@@ -1,16 +1,20 @@
 import request from '@/utils/request'
-// 注册
+// 1.注册
 export const userRegisterService = ({ username, password, repassword }) =>
   request.post('/api/reg', {
     username,
     password,
     repassword
   })
-//登录
+// 2.登录
 export const userLoginService = ({ username, password }) =>
   request.post('/api/login', {
     username,
     password
   })
-//用户信息
+// 3.获取用户信息
 export const userGetInfoService = () => request.get('/my/userinfo')
+// 4.更新用户信息
+export const userUpdateInfoService = ({ id, nickname, email }) => {
+  return request.put('/my/userinfo', { id, nickname, email })
+}
